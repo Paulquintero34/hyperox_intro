5c37fee7a2910f1db9d46b9de1e35cac9a3cd019
Paulquintero34 <opcq16@gmail.com>
2024-06-19 10:28:44 -0600

    operaciones

---
+++ OperacionesBasicos.py
@@ -0,0 +1,17 @@
+class Operaciones:
+    def suma(a,b):
+        return print(a+b)
+    
+    def resta(a,b):
+        return print(a-b)
+    
+    def multiplicacion(a,b):
+        return print(a*b)
+    
+    def division(a,b):
+        return print(a/b)
+    
+Operaciones.suma(2,3)
+Operaciones.resta(2,3)
+Operaciones.multiplicacion(2,3)
+Operaciones.division(2,3)
---
+++ init.py
@@ -0,0 +1,7 @@
+from OperacionesBasicos import Operaciones as op
+
+op.suma(2,3)
+op.resta(2,3)
+op.multiplicacion(2,3)
+op.division(2,3)
+
